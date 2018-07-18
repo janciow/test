63c809ce3d10ee7334560b66e70c29f3545d3b59
Jan Bancarewicz <Jan.Bancarewicz@hrs.com>
2018-07-18 15:59:51 +0200

    understanding the vuejs instance

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'Hello World!',
+    title: 'Hello vue js!',
     link: 'http://google.com',
     finishedLink: '<a href="http://google.com">google</a>'
   },
@@ -49,23 +49,79 @@ new Vue({
     secondCounter: 2
   },
   computed: { // not executed when counter is not changed
-    myOutput: function() {
-      console.log('computed')
+    myOutput: function () {
       return this.counter > 5 ? 'Greater 5' : 'Smaller 5'
     }
   },
   watch: { //Execute code upon data changes
-    counter: function(value) {
+    counter: function (value) {
       var vm = this;
-      setTimeout(function() {
+      setTimeout(function () {
         vm.counter = 0;
       }, 2000)
     }
   },
   methods: {
-    result: function() {
+    result: function () {
       console.log('methods')
       return this.counter > 5 ? 'Greater 5' : 'Smaller 5'
+    }
+  }
+})
+
+
+new Vue({
+  el: '#app5',
+  data: {
+    ingredients: ['meat', 'fruit', 'cookies'],
+    persons: [
+      { name: 'Max', age: 27, color: 'red' },
+      { name: 'Anna', age: 'unknown', color: 'blue' }
+    ]
+  }
+})
+
+
+var vm1 = new Vue({
+  el: '#app6',
+  data: {
+    title: 'the VueJS Instance',
+    showParagrph: false
+  },
+  methods: {
+    show: function () {
+      this.showParagrph = true;
+      this.updateTitle('The VueJS Instace (updated)')
+    },
+    updateTitle: function (title) {
+      this.title = title
+    }
+  },
+  computed: {
+    lowercasleTitle: function() {
+      return this.title.toLowerCase();
+    }
+  },
+  watch: {
+    title: function(value) {
+      alert('Title changed, new value ' + value)
+    }
+  }
+})
+
+setTimeout(function() {
+  vm1.title = 'changed by Timier!'
+  vm1.show();
+}, 3000)
+
+var vm2 = new Vue({
+  el: '#app7',
+  data: {
+    title: 'The second Instuance'
+  },
+  methods: {
+    onChange: function() {
+      vm1.title = 'changed!'
     }
   }
 })
