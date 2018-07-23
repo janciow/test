282b639359451e7cb996bd96ced6314e4012a8fd
Jan Bancarewicz <Jan.Bancarewicz@hrs.com>
2018-07-23 15:40:16 +0200

    An Introduction to Components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,41 @@
 <template>
-<h1>Hello World!</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <app-header></app-header>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+    
+                    <app-servers></app-servers>
+ 
+      
+            <app-server-details></app-server-details>
+
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-xs-12">
+            <app-footer></app-footer>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
-export default {};
+import Servers from "./components/Server/Servers.vue";
+import Header from "./components/Shared/Header.vue";
+import Footer from "./components/Shared/Footer.vue";
+import ServerDetails from "./components/Server/ServerDetails.vue";
+
+export default {
+  components: {
+    appServers: Servers,
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-server-details": ServerDetails
+  }
+};
 </script>
 
-<style lang="scss">
-</style>
