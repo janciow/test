b920bc7a0dc164b27ab053a4b4927e43eb3b158e
Jan Bancarewicz <Jan.Bancarewicz@hrs.com>
2018-07-24 15:32:10 +0200

    Communicating between Components

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,32 +2,56 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my Name</button>
+        <p>Name is {{ name }}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail 
+                :name="name"
+                 @nameWasReset="name = $event"
+                 :resetFn="resetName"
+                 :userAge="age"
+                 ></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit 
+                @ageWasEdited="age = $event"
+                :userAge="age"
+                ></app-user-edit>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import UserDetail from './UserDetail.vue';
-    import UserEdit from './UserEdit.vue';
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
 
-    export default {
-        components: {
-            appUserDetail: UserDetail,
-            appUserEdit: UserEdit
-        }
+export default {
+  data: function() {
+    return {
+      name: "Janek",
+      age: 24
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = "Ana";
+    },
+    resetName() {
+      this.name = "Janek!";
     }
+  },
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit
+  }
+};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightblue;
-    }
+div {
+  background-color: lightblue;
+}
 </style>
